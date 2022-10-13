745876e42aea28bddadc158b56f45e0ffc8e58e2
Erwann Lagouche <popGthyrd@gmail.com>
2022-10-13 15:34:32 +0200

    feat: add more output information
    For clarity sake, as the password might not be asked if is the `sudo` config has persist mode enabled and the user already typed their password in a previous command.

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+echo "info: Installling plymouth-preview"
 install plymouth-preview $HOME/.local/bin/plymouth-preview
 grep -qxF 'PATH="$HOME/.local/bin:${PATH}"' $HOME/.bashrc || echo 'PATH="$HOME/.local/bin:${PATH}"' >> $HOME/.bashrc
 grep -qxF 'export PATH' $HOME/.bashrc || echo 'export PATH' >> $HOME/.bashrc
-echo Introduce your password to create symbolic link
+echo "info: The next step might ask you to introduce your password to create symbolic link"
 sudo ln -sf $HOME/.local/bin/plymouth-preview /usr/local/bin
 source ~/.bashrc
+echo "info: Success. Sourced '~/.bashrc'"
