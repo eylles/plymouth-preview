059a6994063ea62d4b67f026819183bb57cd4e00
Octopus <czirazsombi@icloud.com>
2023-10-23 21:39:47 +0200

    Make install.sh work with other shells and add an uninstall script.
    Also updated text to make it more understandable, and edited the README accordingly to match the modifications.

--- install.sh
+++ install.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
-echo "info: Installling plymouth-preview"
+
+echo "[Info]: Installling plymouth-preview"
+mkdir -p $HOME/.local/bin/
 install plymouth-preview $HOME/.local/bin/plymouth-preview
-grep -qxF 'PATH="$HOME/.local/bin:${PATH}"' $HOME/.bashrc || echo 'PATH="$HOME/.local/bin:${PATH}"' >> $HOME/.bashrc
-grep -qxF 'export PATH' $HOME/.bashrc || echo 'export PATH' >> $HOME/.bashrc
-echo "info: The next step might ask you to introduce your password to create symbolic link"
+
+if [[ $SHELL = *"bash" ]]; then
+	RCPATH=$HOME/.bashrc
+elif [[ $SHELL = *"zsh" ]]; then
+	RCPATH=$HOME/.zshrc
+elif [[ $SHELL = *"fish" ]]; then
+	RCPATH=None
+	fish -c fish_add_path $HOME/.local/bin/
+else
+	echo "[Warning]: Shell is not supported. Please search up a tutorial online on how to add to PATH for your specific shell.
+	The installation is finished, you just have to have ~/.local/bin/ in your PATH."
+	exit 1
+fi
+if [[ $RCPATH != "None" ]]; then
+	grep -qxF 'PATH="$HOME/.local/bin:${PATH}"' $RCPATH || echo 'PATH="$HOME/.local/bin:${PATH}"' >> $RCPATH
+	grep -qxF 'export PATH' $HOME/.bashrc || echo 'export PATH' >> $RCPATH
+	
+	echo "[Info]: Added path '~/.local/bin/' to your .bashrc"
+fi
+
+echo "[Info]: The next step might ask for your password to create a symbolic link."
 sudo ln -sf $HOME/.local/bin/plymouth-preview /usr/local/bin
-source ~/.bashrc
-echo "info: Success. Sourced '~/.bashrc'"
+
+if [[ $SHELL = *"bash" ]]; then source ~/.bashrc
+elif [[ $SHELL = *"zsh" ]]; then source ~/.zshrc
+fi
+echo "[Info]: Successfully installed, enjoy!"
